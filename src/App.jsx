6c9de1066be8db090a2648c4eaba652aad227d38
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-06-23 11:19:03 +0530

    add project section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
@@ -9,10 +10,12 @@ function App() {
       <nav className="flex justify-center space-x-8 py-6 text-lg font-semibold">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
   );
